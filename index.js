1a432091583042df36eb0ef415d3c68110931228
DanPlayz0 <damuffin36@gmail.com>
2022-06-22 08:20:59 +0000

    Relative Config

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('config.js');
+const config = require('./config.js');
 if(!config) throw Error('Config file not found!');
 const client = new Discord.Client({
   intents: ['GUILDS']
